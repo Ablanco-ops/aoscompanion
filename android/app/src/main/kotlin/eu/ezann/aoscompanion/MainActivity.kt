ae386c6f3981059151e217cf3cfea6a6853329d6
Ablanco-opps <ablancopuente@gmail.com>
2021-09-02 12:58:04 +0200

    first commit

---
+++ android/app/src/main/kotlin/eu/ezann/aoscompanion/MainActivity.kt
@@ -0,0 +1,6 @@
+package eu.ezann.aoscompanion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/faction.dart
@@ -0,0 +1,40 @@
+class Faction {
+  final int index;
+  final String name;
+
+  Faction(this.index, this.name);
+
+  List<Faction> _factions = [
+    Faction(101, "Cities of Sigmar"),
+    Faction(102, "Daughters of Khaine"),
+    Faction(103, "Fyreslayers"),
+    Faction(104, "Idoneth Deepkin"),
+    Faction(105, "Kharadron Overlords"),
+    Faction(106, "Lumineth Realmlords"),
+    Faction(107, "Seraphon"),
+    Faction(108, "Strormcast Eternals"),
+    Faction(109, "Sylvaneth"),
+    Faction(201, "Beast of Chaos"),
+    Faction(202, "Blades of Khorne"),
+    Faction(203, "Disciples of Tzeentch"),
+    Faction(204, "Hedonites of Slaanesh"),
+    Faction(205, "Legion of Azgorth"),
+    Faction(206, "Legion of the First Prince"),
+    Faction(207, "Maggotkin of Nurgle"),
+    Faction(208, "Skaven"),
+    Faction(209, "Slaves to Darkness"),
+    Faction(301, "Flesheater Courts"),
+    Faction(302, "Nighthaunt"),
+    Faction(303, "Ossiarch Bonereapers"),
+    Faction(304, "Soulblight Gravelords"),
+    Faction(401, "Gloomspite Gitz"),
+    Faction(402, "Ogor Mawtribes"),
+    Faction(403, "Orruk Warclans"),
+    Faction(404, "Sons of Behemat")  ];
+  List<String> get factions{
+     //return [..._factions];
+    List<String> names=[];
+    _factions.forEach((faction) => names.add(faction.name));
+    return names;
+  }
+}
