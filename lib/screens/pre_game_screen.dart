baf9afd1eb84402a0c05d5eea8c4c3984dc961c8
Ablanco-opps <ablancopuente@gmail.com>
2021-09-02 12:58:04 +0200

    first commit

---
+++ lib/screens/pre_game_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class preGameScreen extends StatefulWidget {
+  const preGameScreen({Key? key}) : super(key: key);
+
+  @override
+  _preGameScreenState createState() => _preGameScreenState();
+}
+
+class _preGameScreenState extends State<preGameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
